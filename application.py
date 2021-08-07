2677425cebe1a3c522fa6d8dc0baad11fd4bd886
ehsansth <ehsanahmed029@gmail.com>
2021-08-07 19:13:03 +0000

    added cache clear, session_permanent set to True

--- application.py
+++ application.py
@@ -20,13 +20,21 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 UPLOAD_FOLDER = 'static/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 # Custom filter
 app.jinja_env.filters["bdt"] = bdt
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
